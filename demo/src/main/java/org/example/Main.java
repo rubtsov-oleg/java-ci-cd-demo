681d3d38559512df111e9e29c800b3e4241678c4
rubtsov-oleg <oleg.rubtsov@factor-esb.ru>
2024-12-30 17:25:50 +0300

    test commit

--- demo/src/main/java/org/example/Main.java
+++ demo/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, CI/CD with Java!");
+        System.out.println("Hello, CI/CD with Java! Test");
     }
 
     public static int add(int a, int b) {
